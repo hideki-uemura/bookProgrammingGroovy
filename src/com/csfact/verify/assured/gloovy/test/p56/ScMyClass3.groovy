66f6992890b58bd86f3ea44d497d57211a6b88fa
hideki-uemura <h-uemura@cs-fact.com>
2016-05-15 03:01:06 +0900

    文字列メソッド呼び出し

---
+++ src/com/csfact/verify/assured/gloovy/test/p56/ScMyClass3.groovy
@@ -0,0 +1,23 @@
+package com.csfact.verify.assured.gloovy.test.p56
+
+class MyClass3{
+    def name = 'nametest'
+    def yobidashi01(){
+        println 'yobidashi01'
+    }
+
+    def yobidashi02(){
+       println "yobidashi02"
+    }
+
+    def yobidashi03(){
+        println "yobidashi03"
+    }
+    String yobidashi01Call(){
+        "yobidashi01"
+    }
+}
+def a = new MyClass3()
+println(a["name"])
+a."yobidashi01"()
+
