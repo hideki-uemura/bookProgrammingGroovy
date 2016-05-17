3b64089cb6432c7083495aaaf2bf88bbefbdc04c
hideki-uemura <h-uemura@cs-fact.com>
2016-05-18 02:38:56 +0900

    study01_package_move

--- src/com/csfact/verify/assured/gloovy/test/chap03/p33/P33Class.groovy
+++ src/com/csfact/verify/assured/gloovy/test/chap03/p33/P33Class.groovy
@@ -3,5 +3,20 @@ package com.csfact.verify.assured.gloovy.test.chap03.p33
 /**
  * Created by uemura on 2016/05/18.
  */
-class P33Class {
+class ScP33Class {
+    def test() {
+        def map = [:].withDefault { 0 }
+        def path = /D:\012_DATA\30_repository\03_GitRepository\programmingGroovy\src\com\csfact\verify\assured\gloovy\test\chap03\p33\input.txt/
+
+        new File(path).eachLine { line ->
+            line.split(/\s+/).each {word->
+                map[word]++
+            }
+        }
+
+        map.entrySet().sort { it.value }.each {
+            println("${it.key} : ${it.value}")
+        }
+    }
 }
+new ScP33Class().test()
